f994de3ba377f19b4fe400d207169fefbab1c793
FlexLevrai <95646927+FlexLevrai@users.noreply.github.com>
2023-09-08 17:39:51 +0000

    az

--- app.js
+++ app.js
@@ -1,42 +1,34 @@
 const express = require('express');
 const morgan = require('morgan');
-
 const webtvRouter = require('./routes/webtvRoutes');
 const webtvRadioRouter = require('./routes/webRadioRoutes');
 const socialMediaRouter = require('./routes/socialMediaRoutes');
 const cors = require('cors');
-
 const bodyParser = require('body-parser');
-
 const app = express();
+const compression = require('compression')
 
 // 1) MIDDLEWARES
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(compression())
+// console.log(process.env.NODE_ENV)
 
-app.use(morgan('dev'));
+if(process.env.NODE_ENV==='developpement'){
+    app.use(morgan('dev'));
+}
 
 
 app.use(express.json());
 //Eneable
 app.use(express.static(`/esmcpanel/v1/webtv`));
-// app.use(express.static(`/esmcpanel/v1/webtv/canton/`));
 app.use(express.static(`/esmcpanel/v1/webradio`));
 app.use(express.static(`/esmcpanel/v1/sm`));
 
 // Eneable Routes for showing images
 app.use('/images/webtv', express.static('images/webtv'));
 app.use('/images/webradio', express.static('images/webradio'));
-
-// app.use( '/images/webtv', express.static(`images`));
-
-// app.use(function(req, res, next) {
-//     // Faire des opérations sur la requête
-//     console.log('Middleware exécuté');
-//     // Passer la main au middleware suivant
-//     next();
-// });
 
 app.use(function(req, res, next) {
     // Faire des opérations sur la requête
@@ -50,4 +42,11 @@ app.use('/esmcpanel/v1/webtv', webtvRouter);
 app.use('/esmcpanel/v1/webradio', webtvRadioRouter);
 app.use('/esmcpanel/v1/sm', socialMediaRouter);
 
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'Faill',
+        massage: `Can't find ${req.originalUrl} on this server!`
+    })
+    // next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 module.exports = app
--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
+dotenv.config({path:'./config.env'})
 const app = require('./app');
 
-dotenv.config({path:'./config.env'});
+console.log(process.env.NODE_ENV)
 
 const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
-
 mongoose
     .connect(DB).then(console.log('DB connect') );
-
-
 const port = process.env.PORT || 3000;
 app.listen(port,() => console.log('App is running on port:' + port + '...'));
-app.get('/', (res, req)=> {
-    res.send(`Welcome to server:  ${port}...s`)
-})
+
+
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+process.on('SIGTERM', () => {
+    console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+    server.close(() => {
+        console.log('💥 Process terminated!');
+    });
+});
